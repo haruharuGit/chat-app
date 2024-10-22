7a0fc29cccf5464746276b4a080c1b633b5038d2
haruharuGit <chiharu.fu.54@gmail.com>
2024-10-22 23:54:51 +0900

    messagesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_22_074759) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_22_144601) do
+  create_table "messages", charset: "utf8mb3", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "room_users", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "room_id"
@@ -39,4 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_22_074759) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
